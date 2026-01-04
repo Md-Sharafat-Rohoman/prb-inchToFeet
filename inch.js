e2912f81a7486aa0f865c50c42a56d157d25ba10
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-01-04 18:22:52 +0600

    'mileToKilometer'

--- inch.js
+++ inch.js
@@ -2,5 +2,20 @@ function inchToFeet(inch){
     const feet = inch/12;
     return feet;
 }
-
 console.log(inchToFeet(75));
+
+function inchToFeet2(inch){
+    const feetRemaining = inch/12;
+    const feetNumber = parseInt(feetRemaining);
+
+    const inchRemaining = inch % 12;
+    const result = feetNumber + ' ft ' + inchRemaining + ' inch';
+    return result;
+}
+console.log(inchToFeet2(80));
+
+function mileTokilometer(mile){
+    const kilometer = mile*1.60934
+    return kilometer;
+}
+console.log(mileTokilometer(4.8))
